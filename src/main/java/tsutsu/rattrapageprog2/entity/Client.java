16c268ad44cf84c9cc0f70913dac0e3b97d0596d
harena <tsutoru24@gmail.com>
2026-09-18 09:35:44 +0300

    add Client and Order entity

---
+++ src/main/java/tsutsu/rattrapageprog2/entity/Client.java
@@ -0,0 +1,79 @@
+package tsutsu.rattrapageprog2.entity;
+
+import java.sql.Date;
+
+public class Client {
+    private int id;
+    private String firstname;
+    private String lastname;
+    private Date dateOfBirth;
+    private String address;
+    private String email;
+    private String phone;
+
+    public Client(String phone, String email, String address, Date dateOfBirth, String lastname, String firstname, int id) {
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+        this.dateOfBirth = dateOfBirth;
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ src/main/java/tsutsu/rattrapageprog2/entity/Order.java
@@ -0,0 +1,53 @@
+package tsutsu.rattrapageprog2.entity;
+
+import org.w3c.dom.Text;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+    private int id;
+    private Timestamp date;
+    private Text description;
+    private List<Clothes> clothes = new ArrayList<Clothes>();
+
+    public Order(int id, List<Clothes> clothes, Text description, Timestamp date) {
+        this.id = id;
+        this.clothes = clothes;
+        this.description = description;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Clothes> getClothes() {
+        return clothes;
+    }
+
+    public void setClothes(List<Clothes> clothes) {
+        this.clothes = clothes;
+    }
+
+    public Text getDescription() {
+        return description;
+    }
+
+    public void setDescription(Text description) {
+        this.description = description;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+}
